66ab45bcf13f33b77bc3983ff4888a48098fc6fb
PIXELL07 <arunksn007@gmail.com>
2026-02-24 13:50:24 +0530

    ps2.py

---
+++ week2/ps2.py
@@ -0,0 +1,28 @@
+# ** Cargo Weight Floor Converter **
+
+# Problem Statement
+# At AeroPort-X, cargo weights are recorded as real numbers for logistical operations.
+# For standardized loading, each recorded weight must be converted into an integer using a fixed rule.
+
+# Task Description
+# You are required to:
+
+# 1) Accept a space-separated sequence of positive real numbers.
+
+# 2) Convert each number to the greatest integer less than or equal to it.
+
+# 3) Print the converted integers separated by commas.
+
+# Input Format
+# A single line containing space-separated real numbers.
+
+# Output Format
+# A single line containing integers separated by commas
+
+# input:
+
+# 1) 100.0 200.8
+# 2) 7.7 8.8 9.9
+# 3) 3.3 3.3 3.3
+
+# code:
