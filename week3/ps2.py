056354fa5b16c3422cf989c06f8150d1b7567577
PIXELL07 <arunksn007@gmail.com>
2026-02-24 15:44:32 +0530

    ps2.py

--- week3/ps2.py
+++ week3/ps2.py
@@ -14,9 +14,26 @@
 
 # input: 496,10,2
 
+# def is_perfect(n):
+#     if n <= 1:
+#         return False
+    
+#     total = 1  
+    
+#     for i in range(2, int(n ** 0.5) + 1):
+#         if n % i == 0:
+#             total += i
+#             if i != n // i:  
+#                 total += n // i
+    
+#     return total == n
+
+# n = int(input())
+# print(is_perfect(n))
+
 # code:
 
-def is_perfect(n):
+def is_perfect(n):                        
     if n <= 1:
         return False
 
