f041742fb6e997039f6230ca26aefa46000413db
PIXELL07 <arunksn007@gmail.com>
2026-02-26 19:16:51 +0530

    ps1.py

---
+++ week5/ps1.py
@@ -0,0 +1,22 @@
+# ** Recursive Multiplication Engine **
+
+# Problem Statement
+
+# In embedded systems at RoboLabs, direct multiplication hardware is unavailable. Engineers must rely on recursive logic based on repeated addition to compute products.
+# You are required to write a Python function as described below.
+
+# 1. multiply(a, b)
+# Input:
+# Two non-negative integers a and b.
+
+# Output:
+# Return the product a × b.
+
+# Notes:
+
+# The computation must be done using recursion.
+# Only addition and subtraction may be used.
+# Loops (for, while) must NOT be used.
+# The * operator must NOT be used.
+
+# code:
