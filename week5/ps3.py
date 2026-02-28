f9066df2a3759a145952f8c69f9c59d0a0267092
PIXELL07 <arunksn007@gmail.com>
2026-03-01 01:35:10 +0530

    ps3.py

---
+++ week5/ps3.py
@@ -0,0 +1,19 @@
+# ** Recursive Minimum Finder **
+
+# Problem Statement
+# Recursive algorithms are often used to process lists when iteration is restricted.
+# You are required to write a Python function as described below.
+
+# * recursive_min(L)
+
+# Input:
+# A non-empty list of integers L.
+
+# Output:
+# Return the minimum element present in the list.
+
+# Notes:
+# The solution must use recursion.
+# Loops must NOT be used.
+# The built-in min() function must NOT be used.
+
