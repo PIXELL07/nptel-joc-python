854374ae5fd9d2ba5e37c08d3366562c4ae9fa03
PIXELL07 <arunksn007@gmail.com>
2026-03-11 23:18:42 +0530

    ps3.py

---
+++ week7/ps3.py
@@ -0,0 +1,21 @@
+# ** Problem Statement: Longest Common Prefix **
+
+# You are given N strings.
+# Your task is to find the longest common prefix shared among all the given strings.
+# A prefix is a sequence of characters that appears at the beginning of each string.
+# If no common prefix exists, print an empty string "".
+
+# Input Format:
+# First line contains an integer N — the number of strings
+# The next N lines each contain one string
+
+# Output Format:
+# Print the longest common prefix
+# If no common prefix exists, print an empty string ""
+
+# Constraints:
+# 1 ≤ N ≤ 200
+# 1 ≤ length of each string ≤ 200
+# Strings contain only lowercase English letters
+
+# code:
