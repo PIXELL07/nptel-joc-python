2c169805ec9627fee5863407ff1205cff0532172
PIXELL07 <arunksn007@gmail.com>
2026-03-11 16:12:05 +0530

    ps2.py

---
+++ week7/ps2.py
@@ -0,0 +1,24 @@
+# ** Problem Statement: Valid Anagram **
+
+# You are given two strings s and t.
+# Your task is to determine whether string t is an anagram of string s.
+
+# Two strings are anagrams if:
+# They contain the same characters
+# Each character appears the same number of times in both strings
+# The order of characters does not matter
+# If t is an anagram of s, print true.
+# Otherwise, print false.
+
+# Input Format:
+# First line contains string s
+# Second line contains string t
+
+# Output Format:
+# Print true if t is an anagram of s
+# Print false otherwise
+
+# Constraints
+# 1 ≤ length of s, t ≤ 5 × 10⁴
+
+# Strings contain only lowercase English letters
